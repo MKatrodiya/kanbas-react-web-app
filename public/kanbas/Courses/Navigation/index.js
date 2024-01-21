b5aa60e2e7d3e2d572a26ff20e0565a96308bc92
Meet <katrodiya.m@northeastern.edu>
2024-01-21 18:38:14 -0500

    Completed courses screen

---
+++ public/kanbas/Courses/Navigation/index.js
@@ -0,0 +1,52 @@
+function CourseNavigation() {
+  return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Navigation</title>
+    </head>
+    <body>
+        <ul>
+        <li><a href="/Kanbas/Courses/Home/screen.html.html">Home</a></li>
+        <li><a href="/Kanbas/Courses/Modules/screen.html.html">Modules</a></li>
+        <li><a href="/Kanbas/Courses/Piazza/screen.html.html">Piazza</a></li>
+        <li>
+            <a href="/Kanbas/Courses/ZoomMeetings/screen.html.html"
+            >Zoom Meetings</a
+            >
+        </li>
+        <li>
+            <a href="/Kanbas/Courses/Assignments/screen.html.html">Assignments</a>
+        </li>
+        <li><a href="/Kanbas/Courses/Quizzes/screen.html.html">Quizzes</a></li>
+        <li><a href="/Kanbas/Courses/Grades/screen.html.html">Grades</a></li>
+        <li><a href="/Kanbas/Courses/People/screen.html.html">People</a></li>
+        <li>
+            <a href="/Kanbas/Courses/PanoptoVideo/screen.html.html"
+            >Panopto Video</a
+            >
+        </li>
+        <li>
+            <a href="/Kanbas/Courses/Discussions/screen.html.html">Discussions</a>
+        </li>
+        <li>
+            <a href="/Kanbas/Courses/Announcements/screen.html.html"
+            >Announcements</a
+            >
+        </li>
+        <li><a href="/Kanbas/Courses/Pages/screen.html.html">Pages</a></li>
+        <li><a href="/Kanbas/Courses/Files/screen.html.html">Files</a></li>
+        <li><a href="/Kanbas/Courses/Rubrics/screen.html.html">Rubrics</a></li>
+        <li><a href="/Kanbas/Courses/Outcomes/screen.html.html">Outcomes</a></li>
+        <li>
+            <a href="/Kanbas/Courses/Collaborations/screen.html.html"
+            >Collaborations</a
+            >
+        </li>
+        <li><a href="/Kanbas/Courses/Settings/screen.html.html">Settings</a></li>
+        </ul>
+    </body>
+    </html>
+
+  `;
+}
