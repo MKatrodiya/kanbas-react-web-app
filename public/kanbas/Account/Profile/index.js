095763f499ce9f46fd5215dfe42067c007f9ef86
Meet <katrodiya.m@northeastern.edu>
2024-01-22 21:22:27 -0500

    Started Accounts screen

---
+++ public/kanbas/Account/Profile/index.js
@@ -0,0 +1,34 @@
+function Profile() {
+  return `
+    <html>
+    <body>
+        <span>Meet Katrodiya's Profile</span>
+        <br />
+        <hr />
+        <br /><br />
+        <table width="100%">
+        <tbody>
+            <tr valign="top">
+            <td>
+                <h1>Meet Katrodiya</h1>
+                <h2>Contact</h2>
+                <span
+                >No registered services, you can add some on the
+                <a href="#">settings</a>
+                page.</span
+                ><br /><br />
+                <h2>Biography</h2>
+                <span>Student, Software Engineer</span><br /><br />
+                <h2>Links</h2>
+                <a href="https://www.youtube.com">YouTube</a>
+            </td>
+            <td>
+                <button type="button">Edit Profile</button>
+            </td>
+            </tr>
+        </tbody>
+        </table>
+    </body>
+    </html>
+    `;
+}
