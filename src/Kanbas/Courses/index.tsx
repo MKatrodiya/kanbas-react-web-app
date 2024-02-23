1eb659314580039137e09f0eee1fcffe82819af5
Meet <katrodiya.m@northeastern.edu>
2024-02-23 00:09:23 -0500

    Minor fixes

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,5 +1,11 @@
 import { courses } from "../../Kanbas/Database";
-import { Navigate, Route, Routes, useLocation, useParams } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useParams,
+} from "react-router-dom";
 import { HiMiniBars3 } from "react-icons/hi2";
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
@@ -10,49 +16,60 @@ import AssignmentEditor from "./Assignments/Editor";
 import Grades from "./Grades";
 
 function Courses() {
-    const { courseId } = useParams();
-    const course = courses.find((course) => course._id === courseId);
-    const { pathname } = useLocation();
-    const paths = pathname.split(/Courses\/[a-zA-Z0-9]+\//)[1];
-    let currentPath = paths.split("/")[0];
-    currentPath = decodeURIComponent(currentPath);
-    const assignmentId = paths?.split("/")?.[1];
-    debugger;
+  const { courseId } = useParams();
+  const course = courses.find((course) => course._id === courseId);
+  const { pathname } = useLocation();
+  const paths = pathname.split(/Courses\/[a-zA-Z0-9]+\//)[1];
+  let currentPath = paths.split("/")[0];
+  currentPath = decodeURIComponent(currentPath);
+  const assignmentId = paths?.split("/")?.[1];
 
-
-    return (
-        <>
-            <div className="wd-breadcrumbs">
-                <div className="d-none d-md-flex ms-4 p-2" style={{ alignItems: "center" }}>
-                    <HiMiniBars3 className="text-danger" />
-                    <div className="ms-2 text-danger align-middle">{course?.name}</div>
-                    <div className={`ms-2 align-middle d-inline`}><FaAngleRight />{currentPath}</div>
-                    {assignmentId && <div className={`ms-2 align-middle d-inline`}><FaAngleRight />{assignmentId}</div>}
-                    <div style={{ marginLeft: "auto" }}>
-                        <button className="btn" style={{ backgroundColor: "#f5f5f5" }}>
-                            <FaGlasses /> Student View
-                        </button>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="wd-breadcrumbs d-none d-md-block">
+        <div
+          className="d-none d-md-flex ms-4 p-2"
+          style={{ alignItems: "center" }}
+        >
+          <HiMiniBars3 className="text-danger" />
+          <div className="ms-2 text-danger align-middle">{course?.name}</div>
+          <div className={`ms-2 align-middle d-inline`}>
+            <FaAngleRight />
+            {currentPath}
+          </div>
+          {assignmentId && (
+            <div className={`ms-2 align-middle d-inline`}>
+              <FaAngleRight />
+              {assignmentId}
             </div>
-            <hr className="mt-2 mb-4" />
-            <div className="d-flex">
-                <CourseNavigation />
-                <div
-                    className="d-block w-100" >
-                    <Routes>
-                        <Route path="/" element={<Navigate to="Home" />} />
-                        <Route path="Home" element={<Home />} />
-                        <Route path="Modules" element={<Modules />} />
-                        <Route path="Piazza" element={<h1>Piazza</h1>} />
-                        <Route path="Assignments" element={<Assignments />} />
-                        <Route path="Assignments/:assignmentId" element={<AssignmentEditor />} />
-                        <Route path="Grades" element={<Grades />} />
-                        <Route path="/*" element={<h1>Not Available</h1>} />
-                    </Routes>
-                </div>
-            </div>
-        </>
-    );
+          )}
+          <div style={{ marginLeft: "auto" }}>
+            <button className="btn" style={{ backgroundColor: "#f5f5f5" }}>
+              <FaGlasses /> Student View
+            </button>
+          </div>
+        </div>
+      </div>
+      <hr className="mt-2 mb-4 d-none d-md-flex" />
+      <div className="d-flex">
+        <CourseNavigation />
+        <div className="d-block w-100">
+          <Routes>
+            <Route path="/" element={<Navigate to="Home" />} />
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Piazza" element={<h1>Piazza</h1>} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route
+              path="Assignments/:assignmentId"
+              element={<AssignmentEditor />}
+            />
+            <Route path="Grades" element={<Grades />} />
+            <Route path="/*" element={<h1>Not Available</h1>} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Courses;
