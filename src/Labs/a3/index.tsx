75226fcad1e8254f498e2a9f0a1ef704aaa911b9
Meet <katrodiya.m@northeastern.edu>
2024-03-19 00:46:54 -0400

    Completed todos lab. Completed lab.

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,27 +1,41 @@
-import Styles from './Styles';
-import Classes from './Classes';
-import JavaScript from './JavaScript';
-import PathParameters from './routing/PathParameters';
-import ConditionalOutput from './ConditionalOutput';
-import Highlight from './HighLight';
-import Add from './Add';
-import TodoList from './todos/TodoList';
+import Styles from "./Styles";
+import Classes from "./Classes";
+import JavaScript from "./JavaScript";
+import PathParameters from "./routing/PathParameters";
+import ConditionalOutput from "./ConditionalOutput";
+import Highlight from "./HighLight";
+import Add from "./Add";
+import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
+import { LabState } from "../store";
 
 function Assignment3() {
-    return (
-        <div className="">
-            <h1>Assignment 3</h1>
-            <TodoList />
-            <Add a={5} b={10} />
-            <Highlight>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio aspernatur alias, nesciunt maiores libero et accusamus, voluptate totam quod, eaque a dignissimos unde at repellendus minima repudiandae nulla nihil dolore.
-            </Highlight>
-            <ConditionalOutput />
-            <Styles />
-            <Classes />
-            <PathParameters />
-            <JavaScript />
-        </div>
-    );
+  const { todos } = useSelector((state: LabState) => state.todosReducer);
+
+  return (
+    <div className="">
+      <h1>Assignment 3</h1>
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+      <TodoList />
+      <Add a={5} b={10} />
+      <Highlight>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
+        aspernatur alias, nesciunt maiores libero et accusamus, voluptate totam
+        quod, eaque a dignissimos unde at repellendus minima repudiandae nulla
+        nihil dolore.
+      </Highlight>
+      <ConditionalOutput />
+      <Styles />
+      <Classes />
+      <PathParameters />
+      <JavaScript />
+    </div>
+  );
 }
 export default Assignment3;
