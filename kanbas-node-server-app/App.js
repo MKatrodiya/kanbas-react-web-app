4a6f9834dabea57e19aca702a2f4bc62ef37ffe9
Meet <katrodiya.m@northeastern.edu>
2024-04-01 01:35:25 -0400

    Completed lab

--- kanbas-node-server-app/App.js
+++ kanbas-node-server-app/App.js
@@ -1,9 +1,13 @@
 import express from "express";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+
 const app = express();
-app.get("/hello", (req, res) => {
-  res.send("Life is good!");
-});
-app.get("/", (req, res) => {
-  res.send("Welcome to Full Stack Development!");
-});
+app.use(cors());
+app.use(express.json());
+
+Hello(app);
+Lab5(app);
+
 app.listen(4000);
