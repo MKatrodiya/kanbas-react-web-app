6824dceff650a18049c45d0fa61f422a5e8dc618
Meet <katrodiya.m@northeastern.edu>
2024-04-18 19:40:38 -0400

    Added user sign in

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -8,12 +8,12 @@ import { useEffect, useState } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
 import axios from "axios";
+import Account from "./Account";
 
 const API_BASE = process.env.REACT_APP_API_BASE;
 
 function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
-  debugger;
   const COURSES_API = `${API_BASE}/api/courses`;
   const findAllCourses = async () => {
     const response = await axios.get(COURSES_API);
@@ -61,7 +61,7 @@ function Kanbas() {
           <div style={{ flexGrow: 1 }}>
             <Routes>
               <Route path="/" element={<Navigate to="Dashboard" />} />
-              <Route path="Account" element={<h1>Account</h1>} />
+              <Route path="Account/*" element={<Account />} />
               <Route
                 path="Dashboard"
                 element={
