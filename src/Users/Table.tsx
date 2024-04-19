29279d4d16802c483f4ae4a6bb0ee56fa6f2cb31
Meet <katrodiya.m@northeastern.edu>
2024-04-19 12:27:05 -0400

    Completed CRUD

--- src/Users/Table.tsx
+++ src/Users/Table.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { BsTrash3Fill, BsPlusCircleFill } from "react-icons/bs";
+import {
+  BsTrash3Fill,
+  BsPlusCircleFill,
+  BsFillCheckCircleFill,
+  BsPencil,
+} from "react-icons/bs";
 import * as client from "./client";
 import { User } from "./client";
 export default function UserTable() {
@@ -32,12 +37,61 @@ export default function UserTable() {
       console.log(err);
     }
   };
+  const deleteUser = async (user: User) => {
+    try {
+      await client.deleteUser(user);
+      setUsers(users.filter((u) => u._id !== user._id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const selectUser = async (user: User) => {
+    try {
+      const u = await client.findUserById(user._id);
+      setUser(u);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const updateUser = async () => {
+    try {
+      const status = await client.updateUser(user);
+      setUsers(users.map((u) => (u._id === user._id ? user : u)));
+      setUser({
+        _id: "",
+        username: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+        role: "USER",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const [role, setRole] = useState("USER");
+  const fetchUsersByRole = async (role: string) => {
+    const users = await client.findUsersByRole(role);
+    setRole(role);
+    setUsers(users);
+  };
   useEffect(() => {
     fetchUsers();
   }, []);
+
   return (
     <div>
       <h1>User Table</h1>
+      <select
+        onChange={(e) => fetchUsersByRole(e.target.value)}
+        value={role || "USER"}
+        className="form-control w-25 float-end"
+      >
+        <option value="USER">User</option>
+        <option value="ADMIN">Admin</option>
+        <option value="FACULTY">Faculty</option>
+        <option value="STUDENT">Student</option>
+      </select>
       <table className="table">
         <thead>
           <tr>
@@ -62,7 +116,7 @@ export default function UserTable() {
                 placeholder="Password"
                 onChange={(e) => setUser({ ...user, password: e.target.value })}
                 required={true}
-                className="form-control"
+                className="form-control mt-1"
               />
             </td>
             <td style={{ verticalAlign: "top" }}>
@@ -96,9 +150,14 @@ export default function UserTable() {
               </select>
             </td>
             <td style={{ verticalAlign: "top", textAlign: "center" }}>
+              <BsFillCheckCircleFill
+                onClick={updateUser}
+                className="me-2 text-success"
+                style={{ fontSize: "2em", cursor: "pointer" }}
+              />
               <BsPlusCircleFill
                 onClick={createUser}
-                className="text-success fomr-control"
+                className="text-success"
                 style={{ fontSize: "2em", cursor: "pointer" }}
               />
             </td>
@@ -111,6 +170,27 @@ export default function UserTable() {
               <td>{user.firstName}</td>
               <td>{user.lastName}</td>
               <td>{user.role}</td>
+              <td style={{ textAlign: "center" }}>
+                <button
+                  onClick={() => deleteUser(user)}
+                  style={{ border: "none", backgroundColor: "transparent" }}
+                >
+                  <BsTrash3Fill
+                    className="text-danger"
+                    style={{ fontSize: "1.25em" }}
+                  />
+                </button>
+                <button
+                  onClick={() => selectUser(user)}
+                  className="btn btn-warning me-2"
+                  style={{ border: "none", backgroundColor: "transparent" }}
+                >
+                  <BsPencil
+                    className="text-primary"
+                    style={{ fontSize: "1.25em" }}
+                  />
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
