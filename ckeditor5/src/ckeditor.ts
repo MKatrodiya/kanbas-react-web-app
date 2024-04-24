d2b65c4329ccfa129560bd6a13a5e016e7dde28f
Meet <katrodiya.m@northeastern.edu>
2024-04-23 20:54:16 -0400

    Added multiple choices question support

---
+++ ckeditor5/src/ckeditor.ts
@@ -0,0 +1,139 @@
+/**
+ * @license Copyright (c) 2014-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import {
+	Bold,
+	Code,
+	Italic,
+	Subscript,
+	Superscript,
+	Underline
+} from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import { CKBox } from '@ckeditor/ckeditor5-ckbox';
+import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
+import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { Highlight } from '@ckeditor/ckeditor5-highlight';
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
+import {
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	PictureEditing
+} from '@ckeditor/ckeditor5-image';
+import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
+import { Link } from '@ckeditor/ckeditor5-link';
+import { List, ListProperties } from '@ckeditor/ckeditor5-list';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+import { Undo } from '@ckeditor/ckeditor5-undo';
+import { WordCount } from '@ckeditor/ckeditor5-word-count';
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends ClassicEditor {
+	public static override builtinPlugins = [
+		Autoformat,
+		BlockQuote,
+		Bold,
+		CKBox,
+		CloudServices,
+		Code,
+		CodeBlock,
+		Essentials,
+		FontColor,
+		FontFamily,
+		FontSize,
+		Heading,
+		Highlight,
+		HorizontalLine,
+		Image,
+		ImageCaption,
+		ImageStyle,
+		ImageToolbar,
+		ImageUpload,
+		Indent,
+		IndentBlock,
+		Italic,
+		Link,
+		List,
+		ListProperties,
+		Paragraph,
+		PasteFromOffice,
+		PictureEditing,
+		Subscript,
+		Superscript,
+		Table,
+		TableToolbar,
+		TextTransformation,
+		Underline,
+		Undo,
+		WordCount
+	];
+
+	public static override defaultConfig: EditorConfig = {
+		toolbar: {
+			items: [
+				'heading',
+				'|',
+				'fontFamily',
+				'fontSize',
+				'fontColor',
+				'highlight',
+				'bold',
+				'italic',
+				'underline',
+				'subscript',
+				'superscript',
+				'bulletedList',
+				'numberedList',
+				'|',
+				'outdent',
+				'indent',
+				'horizontalLine',
+				'|',
+				'link',
+				'blockQuote',
+				'code',
+				'codeBlock',
+				'insertTable',
+				'imageUpload',
+				'undo',
+				'redo'
+			]
+		},
+		language: 'en',
+		image: {
+			toolbar: [
+				'imageTextAlternative',
+				'toggleImageCaption',
+				'imageStyle:inline',
+				'imageStyle:block',
+				'imageStyle:side'
+			]
+		},
+		table: {
+			contentToolbar: [
+				'tableColumn',
+				'tableRow',
+				'mergeTableCells'
+			]
+		}
+	};
+}
+
+export default Editor;
